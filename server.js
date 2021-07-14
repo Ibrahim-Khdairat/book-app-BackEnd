25d5c4d6e100b18dd0d5f885f56abad13ed915c1
Ibrahim-Khdairat <ibrahimkuderat@gmail.com>
2021-07-14 20:30:06 +0300

    unfinishd

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ server.use(express.json());
 
 const PORT = process.env.PORT;
 
-mongoose.connect('mongodb://localhost:27017/best-book', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(`${process.env.MONGO_DB_URL}`, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 const BookSchema = new mongoose.Schema({
